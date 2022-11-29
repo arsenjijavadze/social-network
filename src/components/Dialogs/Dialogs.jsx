c04e5b71e53c8ac6568136eef610b558f8c30092
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2022-11-30 00:00:47 +0300

    feat: add new elements in the dialogs component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,28 @@ import classes from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
     return (
-        <div >
-            Dialogs
+        <div className={classes.dialogs}>
+
+            <div className={classes.dialogsItems}>
+                <div className="dialog">
+                    User1
+                </div>
+                <div className="dialog">
+                    User2
+                </div>
+                <div className="dialog">
+                    User3
+                </div>
+                <div className="dialog">
+                    User4
+                </div>
+            </div>
+            <div className="messages">
+                <div className="message">Hi</div>
+                <div className="message">hello</div>
+                <div className="message">Follow the white rabbit.</div>
+            </div>
+
         </div>
     )
 }
