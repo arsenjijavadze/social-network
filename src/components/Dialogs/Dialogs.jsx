bf5fa0d98e2ddf465236cc7cdbb17984b66dd883
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2022-12-31 03:15:31 +0300

    refactor: change structure of the components in the dialogs and myPotst

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -15,25 +15,37 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        { id: 1, name: 'David' },
+        { id: 2, name: 'Aleksandr' },
+        { id: 3, name: 'Solomon' },
+        { id: 4, name: 'Elisabeth' },
+        { id: 5, name: 'Hanna' },
+        { id: 6, name: 'Mark' },
+    ]
+
+    let messagesData = [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'Follow the white rabbit' },
+        { id: 3, message: 'Hello' },
+        { id: 4, message: 'Hi' },
+        { id: 5, message: 'Hi' },
+
+    ]
+
     return (
         <div className={classes.dialogs}>
 
             <div className={classes.dialogsItems}>
 
-                <DialogItem name="David" id="1" />
-                <DialogItem name="Aleksandr" id="2" />
-                <DialogItem name="Solomon" id="3" />
-                <DialogItem name="Elisabeth" id="4" />
-                <DialogItem name="Hanna" id="5" />
-                <DialogItem name="Mark" id="6" />
-
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
             </div>
             <div className={classes.messages}>
-                <Message message="Hi" />
-                <Message message="Follow the white rabbit" />
-                <Message message="How are you?" />
-                <Message message="Hello" />
-                <Message message="Hi" />
+                <Message message={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
+
 
             </div>
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,13 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
+
+    let postData = [
+        { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+        { id: 2, message: 'It\'s my first post', likesCount: 11 },
+
+    ]
+
     return (
         <div className={classes.postsBlock}>
             <h3>
@@ -19,8 +26,8 @@ const MyPosts = (props) => {
 
             </div>
             <div className={classes.posts}>
-                <Post message='Hi, how are you?' likesCount='0' />
-                <Post message="It's my first post" likesCount='23' />
+                <Post message={postData[0].message} likesCount={postData[0].likesCount} />
+                <Post message={postData[1].message} likesCount={postData[1].likesCount} />
             </div>
         </div>
 
