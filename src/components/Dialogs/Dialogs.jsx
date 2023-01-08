11f1ca2203048a9a3adab22f2919556631123cb0
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-01-08 21:45:31 +0300

    refactor: change structure of the project

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,18 +1,8 @@
 import React from "react";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 import { NavLink } from "react-router-dom";
 import classes from "./Dialogs.module.css"
-
-const DialogItem = (props) => {
-    let path = "/dialogs/" + props.id;
-
-    return <div className={classes.dialog + ' ' + classes.active}>
-        <NavLink to={path}>{props.name}</NavLink>
-    </div>
-}
-
-const Message = (props) => {
-    return <div className={classes.message}>{props.message}</div>
-}
 
 const Dialogs = (props) => {
 
