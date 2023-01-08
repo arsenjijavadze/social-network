5515df75e28c0a87addb8cdcae819aba1a698b8c
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-01-09 01:36:43 +0300

    refactor: move data messages and data dialogs in the index.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = (props) => {
         <div className='app-wrapper-content'>
           <Routes>
             <Route path='/profile' element={<Profile posts={props.posts} />} />
-            <Route path='/dialogs/*' element={<Dialogs />} />
+            <Route path='/dialogs/*' element={<Dialogs dialogs={props.dialogs} messages={props.messages} />} />
             <Route path='/news' element={<News />} />
             <Route path='/music' element={<Music />} />
             <Route path='/settings' element={<Settings />} />
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,28 +6,10 @@ import classes from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
 
-    let dialogs = [
-        { id: 1, name: 'David' },
-        { id: 2, name: 'Aleksandr' },
-        { id: 3, name: 'Solomon' },
-        { id: 4, name: 'Elisabeth' },
-        { id: 5, name: 'Hanna' },
-        { id: 6, name: 'Mark' },
-    ]
 
 
-
-    let messages = [
-        { id: 1, message: 'Hi' },
-        { id: 2, message: 'Follow the white rabbit' },
-        { id: 3, message: 'Hello' },
-        { id: 4, message: 'Hi' },
-        { id: 5, message: 'Hi' },
-
-    ]
-
-    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
-    let messagesElements = messages.map(m => <Message message={m.message} />)
+    let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
+    let messagesElements = props.messages.map(m => <Message message={m.message} />)
 
 
 
