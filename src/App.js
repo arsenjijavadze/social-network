2d5aa9c1f32fc0bc91d7b1a55775eb488b628e93
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-02-05 16:32:47 +0300

    refactor: change structure of the project

--- src/App.js
+++ src/App.js
@@ -17,23 +17,23 @@ const App = (props) => {
 
 
   return (
-    <BrowserRouter>
-      <div className='app-wrapper'>
-        <Header />
-        <Navbar />
-        <div className='app-wrapper-content'>
-          <Routes>
-            <Route path='/profile'
-              element={<Profile state={props.state.profilePage} />} />
-            <Route path='/dialogs/*'
-              element={<Dialogs state={props.state.dialogsPage} />} />
-            <Route path='/news' element={<News />} />
-            <Route path='/music' element={<Music />} />
-            <Route path='/settings' element={<Settings />} />
-          </Routes>
-        </div>
+
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <div className='app-wrapper-content'>
+        <Routes>
+          <Route path='/profile'
+            element={<Profile state={props.state.profilePage} />} />
+          <Route path='/dialogs/*'
+            element={<Dialogs state={props.state.dialogsPage} />} />
+          <Route path='/news' element={<News />} />
+          <Route path='/music' element={<Music />} />
+          <Route path='/settings' element={<Settings />} />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </div>
+
   )
 }
 
