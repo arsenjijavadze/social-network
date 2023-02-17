c6ad0798f608fc8ff3b53adf0d9b195559d1e756
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-02-18 00:12:04 +0300

    refactor: create the store object and change structure of the state

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,76 +1,75 @@
-let rerenderEntireTree = () => {
-    console.log('State changed');
-}
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Hi, how are you? smth', likesCount: 12 },
+                { id: 2, message: 'It\'s my first post', likesCount: 11 },
+                { id: 3, message: 'bla bla', likesCount: 1 },
+            ],
+            newPostText: 'social-network-project'
+        },
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: 'David' },
+                { id: 2, name: 'Aleksandr' },
+                { id: 3, name: 'Solomon' },
+                { id: 4, name: 'Elisabeth' },
+                { id: 5, name: 'Hanna' },
+                { id: 6, name: 'Mark' },
+            ],
+            messages: [
+                { id: 1, message: 'Hi!' },
+                { id: 2, message: 'Follow the white rabbit' },
+                { id: 3, message: 'Hello' },
+                { id: 4, message: 'Hi' },
+                { id: 5, message: 'Hi' },
 
-let state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Hi, how are you? smth', likesCount: 12 },
-            { id: 2, message: 'It\'s my first post', likesCount: 11 },
-            { id: 3, message: 'bla bla', likesCount: 1 },
-        ],
-        newPostText: 'social-network-project'
+            ],
+            newMessageText: 'words'
+        }
     },
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'David' },
-            { id: 2, name: 'Aleksandr' },
-            { id: 3, name: 'Solomon' },
-            { id: 4, name: 'Elisabeth' },
-            { id: 5, name: 'Hanna' },
-            { id: 6, name: 'Mark' },
-        ],
-        messages: [
-            { id: 1, message: 'Hi!' },
-            { id: 2, message: 'Follow the white rabbit' },
-            { id: 3, message: 'Hello' },
-            { id: 4, message: 'Hi' },
-            { id: 5, message: 'Hi' },
+    rerenderEntireTree() {
+        console.log('State changed');
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: state.profilePage.newPostText,
+            likesCount: 0
+        }
+        state.profilePage.posts.push(newPost);
+        state.profilePage.newPostText = '';
+        rerenderEntireTree(state);
+    },
+    updateNewPostText(newText) {
 
-        ],
-        newMessageText: 'words'
+        state.profilePage.newPostText = newText;
+        rerenderEntireTree(state);
+    },
+    addMessage() {
+        let newMessage = {
+            id: 6,
+            message: state.dialogsPage.newMessageText,
+        }
+        state.dialogsPage.messages.push(newMessage);
+        state.dialogsPage.newMessageText = '';
+        rerenderEntireTree(state);
+    },
+
+    updateNewMessageText(newText) {
+
+        state.dialogsPage.newMessageText = newText;
+        rerenderEntireTree(state);
+    },
+    subscribe(observer) {
+        rerenderEntireTree = observer;
     }
 }
 
-// posts
-
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    }
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
 
 
-export const updateNewPostText = (newText) => {
 
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
 
-//messages
-export const addMessage = () => {
-    let newMessage = {
-        id: 6,
-        message: state.dialogsPage.newMessageText,
-    }
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.newMessageText = '';
-    rerenderEntireTree(state);
-}
 
-export const updateNewMessageText = (newText) => {
-
-    state.dialogsPage.newMessageText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
+window.store = store;
